c0b36e45985df14cec8c604a9931fd91f46e557e
RuanKlinkert <u14022282@tuks.co.za>
2016-08-24 16:11:05 +0200

    Update Graph.cs
    Added the epi-use colours

--- Vizard/Assets/Graph.cs
+++ Vizard/Assets/Graph.cs
@@ -1608,8 +1608,11 @@ public class Graph {
 	#region Tools
 	//Default colors
 	private Color[] colors = new Color[]{ 
+		new Color32((byte)207,(byte)17,(byte)63,(byte)255),
+		new Color32((byte)11,(byte)28,(byte)56,(byte)255),
+		new Color32((byte)82,(byte)207,(byte)41,(byte)255),
 		new Color32((byte)91,(byte)155,(byte)213,(byte)255),
-		new Color32((byte)237,(byte)125,(byte)49,(byte)255),
+		new Color32((byte)207,(byte)93,(byte)17,(byte)255),
 		new Color32((byte)165,(byte)165,(byte)165,(byte)255),
 		new Color32((byte)255,(byte)192,(byte)0,(byte)255),
 		new Color32((byte)68,(byte)114,(byte)196,(byte)255),
